6f4bb572ab266f14e3fe1c3f9120a5d10ebc0100
Nibir <mz.nibir@gmail.com>
2021-05-28 00:39:09 +0600

    creating registation form in authentication

--- My_Second_Project/Login_app/urls.py
+++ My_Second_Project/Login_app/urls.py
@@ -5,5 +5,6 @@ from Login_app import views
 app_name = 'Login_app'
 
 urlpatterns= [
-    path('', views.index, name='index' )
+    path('', views.index, name='index' ),
+    path('register/', views.register, name='register')
 ]
