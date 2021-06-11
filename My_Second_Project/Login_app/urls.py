afbc69ba1a03637ce5ca1b7821f2487d3d2d5da6
Nibir <mz.nibir@gmail.com>
2021-06-11 22:10:38 +0600

    user login part-1

--- My_Second_Project/Login_app/urls.py
+++ My_Second_Project/Login_app/urls.py
@@ -6,5 +6,7 @@ app_name = 'Login_app'
 
 urlpatterns= [
     path('', views.index, name='index' ),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login_page, name='login'),
+    path('user_login/', views.user_login, name='user_login'),
 ]
