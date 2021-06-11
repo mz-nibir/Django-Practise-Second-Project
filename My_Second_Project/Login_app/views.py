f066f35bed3727b9368d0085ae9196f8a5268653
Nibir <mz.nibir@gmail.com>
2021-06-11 14:55:08 +0600

    Register user

--- My_Second_Project/Login_app/views.py
+++ My_Second_Project/Login_app/views.py
@@ -8,8 +8,30 @@ def index(request):
     return render(request,'Login_app/index.html', context=dict)
 
 def register(request):
-    user_form= UserForm()
-    user_info_form= UserInfoForm()
+    registered = False
 
-    dict={'user_form':user_form,'user_info_form':user_info_form}
+    if request.method == 'POST':
+
+        user_form= UserForm(data=request.POST)
+        user_info_form= UserInfoForm(data= request.POST)
+
+        if user_form.is_valid() and user_info_form.is_valid():
+            user= user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            user_info= user_info_form.save(commit=False)
+            user_info.user= user
+
+            if 'profile_pic' in request.FILES:
+                user_info.profile_pics= request.FILES['profile_pic']
+
+            user_info.save()
+            registered= True
+
+    else:
+        user_form= UserForm()
+        user_info_form= UserInfoForm()
+
+    dict={'user_form':user_form,'user_info_form':user_info_form,'registered':registered}
     return render(request,'Login_app/register.html', context=dict)
